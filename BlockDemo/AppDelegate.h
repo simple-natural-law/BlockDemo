80e1a1034dc750f5fdbf8dfe6e774c3ae62d12a7
张诗健 <zsjcwy_xcode@163.com>
2018-03-13 17:48:15 +0800

    commit

---
+++ BlockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockDemo
+//
+//  Created by 讯心科技 on 2018/3/13.
+//  Copyright © 2018年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlockDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockDemo
+//
+//  Created by 讯心科技 on 2018/3/13.
+//  Copyright © 2018年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
